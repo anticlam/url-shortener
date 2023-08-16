0711c0b6278de194a9a163ff76c6c8e21588be8d
Abdulsalam <141822082+anticlam@users.noreply.github.com>
2023-08-16 13:03:59 +0300

    Delete shortener directory

--- shortener/urls.py
+++
@@ -1,9 +0,0 @@
-from shortener.views import ShortenerCreateApiView, ShortenerListAPIView
-from django.urls import path, include
-
-app_name = 'my_shortener_app'
-
-urlpatterns = [
-    path('',ShortenerListAPIView.as_view(),name='all_links'),
-    path('create/',ShortenerCreateApiView.as_view(),name='create_api'),
-]
