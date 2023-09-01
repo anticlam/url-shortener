b01285b9f1dffc002fbe738cf8b899bd75180361
flow <climac@proton.me>
2023-09-01 15:17:29 +0300

    Added support for hashing and cache

--- shortener/admin.py
+++ shortener/admin.py
@@ -1,20 +1,23 @@
 from django.contrib import admin
 from .models import Link
 
-# Regestiring the Link model in the admin site.
+
 @admin.register(Link)
 class LinkAdmin(admin.ModelAdmin):
-    list_display=['original_link','shortened_link', 'created_at', 'id']
-    search_fields=['original_link','shortened_link', 'created_at', 'id']
-    list_filter=['original_link','shortened_link', 'created_at', 'id']
-    readonly_fields=['shortened_link']
-    fieldsets=(
-        ('Link',{
-            'fields':(
-                'original_link',
-                'shortened_link',
-                'created_at',
-                'id',
-            )
-        }),
+    list_display = ["original_link", "shortened_link", "created_at", "id"]
+    search_fields = ["original_link", "shortened_link", "created_at", "id"]
+    list_filter = ["original_link", "shortened_link", "created_at", "id"]
+    readonly_fields = ["shortened_link"]
+    fieldsets = (
+        (
+            "Link",
+            {
+                "fields": (
+                    "original_link",
+                    "shortened_link",
+                    "created_at",
+                    "id",
+                )
+            },
+        ),
     )
