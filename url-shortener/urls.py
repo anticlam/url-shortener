cc1bf2cf1cf31688f87625626a03919f14732799
flow <climac@proton.me>
2023-09-01 12:33:23 +0300

    updated views to use drf viewset

--- url-shortener/urls.py
+++ url-shortener/urls.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include 
-from shortener.views import Redirector
-
+from django.urls import path, include
+from shortener.views import LinkViewSet  
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shortener/', include(('shortener.urls', 'my_shortener_app'), namespace='my_shortener_namespace')),
-    path('<str:shortener_link>/', Redirector.as_view(), name='redirector'),
+    path('<str:shortener_link>/', LinkViewSet.as_view({'get': 'redirector'}), name='redirector'),  # Update this line
 
     # API routes
-    path('api/users/', include('user.urls')),  # Note: We'll modify user.urls accordingly in the next step
+    path('api/users/', include('user.urls')),
 
     # Uncomment below if you want to keep the old route (without the API prefix) alongside the new one
     # path("user/", include("user.urls")),
