f38addce1735acebd919e206fdcfa59f4b3e7fac
flow <climac@proton.me>
2023-09-01 12:33:45 +0300

    update to use viewsets

--- shortener/views.py
+++ shortener/views.py
@@ -1,26 +1,29 @@
-# Standard library imports
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from django.shortcuts import redirect, Http404
 from django.conf import settings
 
-# Third-party imports
-from rest_framework import generics, status
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from rest_framework.response import Response
-
-# Local application imports
 from .models import Link
 from .serializer import LinkSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-
-class ShortenerListAPIView(generics.ListAPIView):
-    permission_classes = [IsAdminUser]
-    queryset = Link.objects.all()
+class LinkViewSet(viewsets.ModelViewSet):
     serializer_class = LinkSerializer
 
+    def get_permissions(self):
+        if self.action == 'list':
+            self.permission_classes = [IsAdminUser]
+        elif self.action == 'create':
+            self.permission_classes = [IsAuthenticated]
+        else:
+            self.permission_classes = []
+        return [permission() for permission in self.permission_classes]
 
-class ShortenerCreateAPIView(generics.CreateAPIView):
-    permission_classes = [IsAuthenticated]
-    serializer_class = LinkSerializer
+    def get_queryset(self):
+        if self.action == 'list':
+            return Link.objects.all()
+        return Link.objects.none()
 
     def create(self, request, *args, **kwargs):
         original_link = request.data.get('original_link')
@@ -32,9 +35,8 @@ class ShortenerCreateAPIView(generics.CreateAPIView):
         link = serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-class Redirector(generics.GenericAPIView):
-    permission_classes = []
-    def get(self, request, shortener_link, *args, **kwargs):
+    @action(detail=False, methods=['GET'], url_path='(?P<shortener_link>.+)')
+    def redirector(self, request, shortener_link):
         shortener_link = settings.HOST_URL + '/' + shortener_link
         redirect_link = Link.objects.filter(shortened_link=shortener_link).first()
         if redirect_link:
