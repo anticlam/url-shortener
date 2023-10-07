449585bdfa9d7800ae72f0432b7efd165ff2da37
climac <climac@proton.me>
2023-10-07 23:53:05 +0300

    Updated tests and requirements

--- shortener/tests.py
+++ shortener/tests.py
@@ -1,34 +1,64 @@
+
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-from shortener.models import Link
-from user.models import User
 
+class ShortenerTests(APITestCase):
 
-class LinkTests(APITestCase):
+    def obtain_token(self):
+        # create a test user
+        user_data = {
+            "email": "testuser@example.com",
+            "password": "testpassword"
+        }
+        self.client.post(reverse('user-signup-list'), user_data, format='json')
+        
+        # obtain a token for the test user
+        response = self.client.post(reverse('login'), user_data, format='json')
+        return response.data['access']
+    
     def setUp(self):
-        self.superuser = User.objects.create_superuser("admin@example.com", "password")
-        self.client.login(email="admin@example.com", password="password")
+        self.token = self.obtain_token()
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
 
-    def test_create_shortened_link(self):
-        url = reverse("my_shortener_namespace:create_api")
-        data = {"original_link": "https://www.example.com"}
-        response = self.client.post(url, data, format="json")
+
+    def test_url_shortening(self):
+        # test if link redirects to correct website
+        url = reverse('my_shortener_namespace:create_api')
+        data = {'original_link': 'https://www.example.com/long-url'}
+        response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Link.objects.count(), 1)
-        self.assertEqual(Link.objects.get().original_link, "https://www.example.com")
+        self.assertIn('shortened_link', response.data)
 
-    def test_list_shortened_links(self):
-        Link.objects.create(original_link="https://www.example1.com")
-        Link.objects.create(original_link="https://www.example2.com")
-        url = reverse("my_shortener_namespace:all_links")
-        response = self.client.get(url)
-        print(f"Debug: Response Data in test_list_shortened_links: {response.data}")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data["results"]), 2)
+    def test_access_shortened_url(self):
+        url = reverse('my_shortener_namespace:create_api')
+        data = {'original_link': 'https://www.example.com/long-url'}
+        shortened_response = self.client.post(url, data, format='json')
+        shortened_link = shortened_response.data['shortened_link']
+        
+        # check redirection
+        response = self.client.get(reverse('redirector', args=[shortened_link.split('/')[-1]]))
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+        self.assertEqual(response.url, data['original_link'])
 
-    def test_redirect_link_not_found(self):
-        url = reverse("redirector", args=["nonexistent"])
-        response = self.client.get(url)
-        print(f"Debug: Response in test_redirect_link_not_found: {response.__dict__}")
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    def test_existing_shortened_url(self):
+        url = reverse('my_shortener_namespace:create_api')
+        data = {'original_link': 'https://www.example.com/long-url'}
+        
+        # Create link for the first time
+        response_first = self.client.post(url, data, format='json')
+        shortened_link_first = response_first.data['shortened_link']
+
+        # Create same link again
+        response_second = self.client.post(url, data, format='json')
+        shortened_link_second = response_second.data['shortened_link']
+
+        # Test if they are the same
+        self.assertEqual(shortened_link_first, shortened_link_second)
+
+    def test_invalid_url(self):
+        url = reverse('my_shortener_namespace:create_api')
+        data = {'original_link': 'invalid-url'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('original_link', response.data)
--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class UserTests(APITestCase):
+
+    def setUp(self):
+        self.signup_url = reverse('user-signup-list')
+        self.login_url = reverse('login')
+        self.user_data = {'email': 'testuser@example.com', 'password': 'testpass123'}
+
+        # Create a user 
+        self.client.post(self.signup_url, self.user_data, format='json')
+
+    def test_user_creation(self):
+        data = {'email': 'newuser@example.com', 'password': 'newpass123'}
+        response = self.client.post(self.signup_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertIn('email', response.data['data'])
+
+    def test_user_login(self):
+        response = self.client.post(self.login_url, self.user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('access', response.data)
+
+    def test_token_validation(self):
+        # Get token
+        login_response = self.client.post(self.login_url, self.user_data, format='json')
+        token = login_response.data['access']
+
+        # Test authentication 
+        headers = {'HTTP_AUTHORIZATION': f'Bearer {token}'}
+        response = self.client.get(reverse('user-signup-list'), **headers)
+        
+        # Make sure only admin has access
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        # Test with bad token
+        headers_invalid = {'HTTP_AUTHORIZATION': 'Bearer invalid_token'}
+        response_invalid = self.client.get(reverse('user-signup-list'), **headers_invalid)
+        self.assertEqual(response_invalid.status_code, status.HTTP_403_FORBIDDEN)
